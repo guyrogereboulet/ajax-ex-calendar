612b30c0cbf7f6a5da9b3e2d027b02839cfa1f75
Guy Roger Eboulet <eboulet87@gmail.com>
2020-02-03 11:57:11 +0100

    Inserimento script handlebars

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
-  
+
+  var url = ""
+
+  $.ajax(
+   {
+   url: "http://www.boolean.careers/api/random/boolean",
+   method: "GET",
+   success: function (data, stato) {
+   $("#risultati").html(data);
+   },
+   error: function (richiesta, stato, errori) {
+   alert("E' avvenuto un errore. " + errore);
+   }
+   }
+  );
 
 });
